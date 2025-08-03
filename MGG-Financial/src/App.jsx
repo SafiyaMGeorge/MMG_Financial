14183d6f963325d1e0b9f1d3c918174797ac8372
Safiya George <smg_dev@outlook.com>
2025-08-02 22:27:21 -0400

    Added router to project.

--- MGG-Financial/src/App.jsx
+++ MGG-Financial/src/App.jsx
@@ -1,11 +1,26 @@
 
-import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Annuities from "./pages/Annuities";
+import Insuurance from "./pages/Insurance";
+import Investments from "./pages/Investments";
+import Retirement from "./pages/Retirement";
 function App() {
 
   return (
     <>
       hello world
+      <Router basename="/MGG-Financial/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/annuities" element={<Annuities />} />
+        <Route path="/insurance" element={<Insuurance />} />
+        <Route path="/investments" element={<Investments />} />
+        <Route path="/retirement" element={<Retirement />} />
+      </Routes>
+    </Router>
     </>
   )
 }
