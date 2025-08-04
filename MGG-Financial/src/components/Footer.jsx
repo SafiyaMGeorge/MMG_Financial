9ee1f7d0c9c94e1118390ab2b5897eb28fc09ee7
Safiya George <smg_dev@outlook.com>
2025-08-04 13:17:44 -0400

    Added font imported fontawesome icons. I also separated the footer into three differnt sections for the respective content. Added some styling as well as a background color to make suer the correct section was selected to work on. Having some isses with the footer width.

--- MGG-Financial/src/components/Footer.jsx
+++ MGG-Financial/src/components/Footer.jsx
@@ -1,19 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import logo from '../assets/Media/MGG-logo.svg'
 
+library.add(fas, far, fab)
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="container">
-        <secton className="social-icons">
-          
+    <footer className="footer w-full">
+      <div className="container w-full bg-red-900">
+        <div className="content flex justify-between items-center bg-blue-900">
+          <secton className="social-icons">
+          <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+          <FontAwesomeIcon icon="fa-brands fa-square-facebook" />
         </secton>
         <section className="footer-logo">
+          <img src={logo} alt="MGG Financial Logo" />
         </section>
         <section className="footer-links">
-          <a href="/">Home</a>
-          <a href="/about">About Us</a>
+          <ul className="flex flex-col md:flex-row gap-4 ">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About Us</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
         </section>
-        <p>&copy; {new Date().getFullYear()} MGG Financial. All rights reserved.</p>
+        </div>
+        <p className=" w-full text-center">&copy; {new Date().getFullYear()} MGG Financial. All rights reserved.</p>
       </div>
     </footer>
   );
