52d69abff1e23f360a86205443ccc3ba1f2a4271
Safiya George <smg_dev@outlook.com>
2025-08-03 23:37:56 -0400

    Added Footer component. removed basename in router and added dive with classname. Lastly, corrected the word insurance in both imports section and router.

--- MGG-Financial/src/App.jsx
+++ MGG-Financial/src/App.jsx
@@ -1,23 +1,26 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Annuities from "./pages/Annuities";
-import Insuurance from "./pages/Insurance";
+import Insurance from "./pages/Insurance";
 import Investments from "./pages/Investment";
 import Retirement from "./pages/Retirement";
+import Footer from "./components/Footer";
 function App() {
 
   return (
-      <Router basename="/MGG-Financial/">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/annuities" element={<Annuities />} />
-        <Route path="/insurance" element={<Insuurance />} />
-        <Route path="/investments" element={<Investments />} />
-        <Route path="/retirement" element={<Retirement />} />
-      </Routes>
+    <Router>
+      <div className="MGG-Financial">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/annuities" element={<Annuities />} />
+          <Route path="/insurance" element={<Insurance />} />
+          <Route path="/investments" element={<Investments />} />
+          <Route path="/retirement" element={<Retirement />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   )
 }
