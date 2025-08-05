baaccbb0de4a6b51cc42cf81b9f6240fc53b95dd
Safiya George <smg_dev@outlook.com>
2025-08-05 19:22:25 -0400

    I added the hero banner picture into the media folder and then imported into page. I removed the starter h1 and p element.

--- MGG-Financial/src/pages/Home.jsx
+++ MGG-Financial/src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+
+import heroImg from'../assets/Media/eva-darron.jpg';
+
 function Home() {
   return (
     <div className="home">
-      <h1>Welcome to MGG Financial</h1>
-      <p>Your trusted partner in financial planning and investment strategies.</p>
+      <main className="main-content">
+        <section className="hero-section h-65 flex bg-cover bg-center" style={{ backgroundImage: `url(${heroImg})` }}>
+          <h1 className=" w-45 ml-50 text-white text-2xl absolute top-45 right-170">Plan for your dream future</h1>
+        </section>
+        <section className="service-section h-50 flex items-center justify-center bg-gray-100">This is the service section</section>
+        <div>This section is an info section </div>
+        <section className="about-section h-50 flex items-center justify-center bg-gray-100">This is the about section</section>
+        <div>Another info section</div>
+        <section className="reasons-section h-50 flex items-center justify-center bg-gray-100">This is a reason section</section>
+        <section className="product-section h-50 flex items-center justify-center bg-gray-100">This is the product section</section>
+      </main>
     </div>
   );
 }
