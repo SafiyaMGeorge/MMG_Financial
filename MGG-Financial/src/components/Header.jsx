837f9df00929137732749fb2f91f219c9bd2aa76
Safiya-Yourbookteam-website-intern <safiyayourbookteam@gmail.com>
2025-08-27 13:57:43 -0400

    Updated the About Us to About.

--- MGG-Financial/src/components/Header.jsx
+++ MGG-Financial/src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
           <section className="header-links flex gap-16 lg:gap-28">
             <ul className="flex flex-col md:flex-row gap-10 mr-8 text-white">
                 <li><a href="/">Home</a></li>
-                <li><a href="/about">About Us</a></li>
+                <li><a href="/about">About</a></li>
                 <li>
                     <div className="flex relative group">
                         <div className="border-none bg-none cursor-pointer flex gap-2 items-center">
