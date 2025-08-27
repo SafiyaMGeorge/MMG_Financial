46554fdd530a309f1c1b1dd3e3d214cef2b1f0bb
Safiya-Yourbookteam-website-intern <safiyayourbookteam@gmail.com>
2025-08-27 13:29:59 -0400

    created folder to store additional data such as product data.

---
+++ MGG-Financial/src/data/ProductCarousel.js
@@ -0,0 +1,26 @@
+import businessImg from '../assets/Media/Business.png';
+
+const productData = [
+        {   
+            "id": 1,
+            "name": "Insurance",
+            "description": "Affordable coverage for a specific term, ideal for temporary needs.",
+            "image": businessImg,
+            "link" : "/life-insurance"
+        },
+        {
+            "id": 2,
+            "name": "Investments",
+            "description": "Lifetime coverage with cash value accumulation.",
+            "link" : "/annuities"
+        },
+        {
+            "id": 3,
+            "name": "Retirement",
+            "description": "help clients save for retirement by selling various tax-advantaged accounts and funds.",
+            "link" : "/investments"
+        }
+
+    ];
+
+export default productData;
