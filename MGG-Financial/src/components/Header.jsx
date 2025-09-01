b6e2999d32b2c998392263345c64ab71d6749d93
Safiya <smg_dev@outlook.com>
2025-08-31 21:00:06 -0400

    Updated the header to navigate to correct page. I also removed the dropdown as I no longer going to individual pages for each service.

--- MGG-Financial/src/components/Header.jsx
+++ MGG-Financial/src/components/Header.jsx
@@ -13,30 +13,9 @@ function Header() {
             <ul className="flex flex-col md:flex-row gap-10 mr-8 text-white">
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
-                <li>
-                    <div className="flex relative group">
-                        <div className="border-none bg-none cursor-pointer flex gap-2 items-center">
-                            <a href="/annuities">Products</a>
-                        </div>
-                        <div className="hidden group-hover:flex absolute top-full w-23 bg-[#0B2E46]  py-4 rounded flex-col gap-1 text-white text-center">    
-                            <a href="/annuities">Annuities</a>
-                            <div className="border-b border-white -mx-2"></div>
-                            <a href="/annuities">Insurance</a>
-                        </div>
-                    </div>
+                <li><a href="/products">Products</a>
                 </li>
-                <li>
-                    <div className="flex relative group">
-                        <div className="border-none bg-none cursor-pointer flex gap-2 items-center">
-                            <a href="/annuities">Services</a>
-                        </div>
-                        <div className="hidden group-hover:flex absolute top-full w-26 bg-[#0B2E46] py-4 rounded flex-col gap-1 text-white text-center">    
-                            <a href="/annuities">Investments</a>
-                            <div className="border-b border-white -mx-2"></div>
-                            <a href="/annuities">Retirement</a>
-                        </div>
-                    </div>
-                </li>
+                <li><a href="/services">Services</a></li>
             </ul>
           </section>
         </div>
