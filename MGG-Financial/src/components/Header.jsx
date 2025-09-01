8d139fef512a12b66b35e485e6a815e22b944261
Safiya <smg_dev@outlook.com>
2025-08-31 21:02:13 -0400

    place ending li tag on same line as starting tag.

--- MGG-Financial/src/components/Header.jsx
+++ MGG-Financial/src/components/Header.jsx
@@ -13,8 +13,7 @@ function Header() {
             <ul className="flex flex-col md:flex-row gap-10 mr-8 text-white">
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
-                <li><a href="/products">Products</a>
-                </li>
+                <li><a href="/products">Products</a></li>
                 <li><a href="/services">Services</a></li>
             </ul>
           </section>
