e6bdb07062f719d11b09d0877e69be79fe7cabdd
SafiyaMGeorge <smg_dev@outlook.com>
2025-11-18 12:21:13 -0500

    Update field input

--- MGG-Financial/src/data/ProductCarousel.js
+++ MGG-Financial/src/data/ProductCarousel.js
@@ -2,23 +2,23 @@ import businessImg from '../assets/Media/Business.png';
 
 const productData = [
         {   
-            "id": 1,
-            "name": "Insurance",
-            "description": "Affordable coverage for a specific term, ideal for temporary needs.",
-            "image": businessImg,
-            "link" : "/life-insurance"
+            id: 1,
+            name: "Insurance",
+            description: "Affordable coverage for a specific term, ideal for temporary needs.",
+            image: businessImg,
+            link : "/life-insurance"
         },
         {
-            "id": 2,
-            "name": "Investments",
-            "description": "Lifetime coverage with cash value accumulation.",
-            "link" : "/annuities"
+            id: 2,
+            name: "Investments",
+            description: "Lifetime coverage with cash value accumulation.",
+            link : "/annuities"
         },
         {
-            "id": 3,
-            "name": "Retirement",
-            "description": "help clients save for retirement by selling various tax-advantaged accounts and funds.",
-            "link" : "/investments"
+            id: 3,
+            name: "Retirement",
+            description: "help clients save for retirement by selling various tax-advantaged accounts and funds.",
+            link : "/investments"
         }
 
     ];
