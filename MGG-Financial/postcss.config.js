85995d818aca8f90bd7df6745f5cd58693c2360d
SafiyaMGeorge <smg_dev@outlook.com>
2025-11-18 12:21:59 -0500

    Created config file for tailwindcss and postcss

---
+++ MGG-Financial/postcss.config.js
@@ -0,0 +1,8 @@
+export default {
+  plugins: {
+    // Other PostCSS plugins you might have
+    // "postcss-import": {}, // If you were using postcss-import, it's often handled automatically by @tailwindcss/postcss in v4+
+    // autoprefixer: {}, // If you were using autoprefixer, it's often handled automatically by @tailwindcss/postcss in v4+
+    "@tailwindcss/postcss": {},
+  },
+};
