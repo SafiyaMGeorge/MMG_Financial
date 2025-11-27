105c79afcd06202fa51d85ff136989fed0e1fd1f
SafiyaMGeorge <smg_dev@outlook.com>
2025-11-26 22:25:36 -0500

    Removed the use state and the manual carousel function as I decided to use the embal carousel library to generate the carousel. I also added responsive sizing Tailwindcss classes.

--- MGG-Financial/src/components/carousel.jsx
+++ MGG-Financial/src/components/carousel.jsx
@@ -1,48 +1,40 @@
-import React, {useState} from "react";
-import { useNavigate } from 'react-router-dom';
-import insuranceData from "../data/insuranceData";
-//This component takes in an object with the carosel items. This way the list of items can be edited without changing the component
+import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import AutoScroll from "embla-carousel-auto-scroll";
 
-function Carousel(){
-    //building carosel function
-     const [currentIndex, setCurrentIndex] = useState(0);
-     const visibleItems = 3;
-     const navigate = useNavigate();
-     
-    const prevSlide = () => {
-     setCurrentIndex((prev) =>
-          Math.max(prev - visibleItems, 0)
-        );
-    };
+function Carousel({ data }) {
+  const [emblaRef] = useEmblaCarousel(
+    {
+      loop: true,
+      align: "start",
+    },
+    [
+      AutoScroll({
+        playOnInit: true,
+        speed: 1,
+        stopOnHover: true,
+        stopOnInteraction: false,
+      }),
+    ]
+  );
 
-    const nextSlide = () => {
-     setCurrentIndex((prev) =>
-          Math.min(prev + visibleItems, insuranceData.length - visibleItems)
-        );
-    };
-
-    const visible = insuranceData.slice(currentIndex, currentIndex + visibleItems);
-    const handleClick = (link) => {
-          navigate(link);
-  };
-
-    return(
-        <div className="carousel-container w-full h-68 flex justify-between items-center">
-            <button onClick={prevSlide} disabled={currentIndex === 0} className="ml-5 cursor-pointer">
-                 ◀
-            </button>
-            <div className="carousel h-60 flex items-center overflow-hidden">
-                 {visible.map((item, index) => (
-                    <div onClick={() => handleClick(item.link)} className="carousel-item w-90 h-full mx-2 text-center cursor-pointer" key={index}>
-                        {item.image && <img src={item.image} alt={item.name} className="w-full h-48 object-cover mb-2" />}
-                        <p className="mt-6">{item.name}</p>
-                    </div>
-                ))}
-            </div>
-            <button onClick={nextSlide} disabled={currentIndex >= insuranceData.length - visibleItems} className="mr-5 cursor-pointer">
-                ▶
-            </button>
-        </div>
-    );
+  return (
+    <div className="overflow-hidden w-full py-6" ref={emblaRef}>
+      <div className="flex">
+        {data.map((item, i) => (
+          <div
+            key={i}
+            className="embla__slide shrink-0 mr-10 flex items-center w-60 md:w-125 md:h-80 bg-linear-to-b from-[#4A90E2] to-[#0C243C] text-white rounded-2xl py-2">
+            <section className="flex flex-col items-center text-center">
+              <img src={item.image} alt="" className="w-21 md:w-42 mb-5" />
+              <h3 className="mb-3 text-md md:text-lg">{item.name}</h3>
+              <p className="leading-relaxed md:px-10 text-sm md:text-md">{item.description}</p>
+            </section>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
 export default Carousel;
