169d5b0f6d17461f81725ac48d9e63c24418dff5
SafiyaMGeorge <smg_dev@outlook.com>
2025-11-26 22:30:06 -0500

    I imported new images for carousel cards. I changed the names and descriptions and added the rest of the images.

--- MGG-Financial/src/data/ProductCarousel.js
+++ MGG-Financial/src/data/ProductCarousel.js
@@ -1,24 +1,40 @@
-import businessImg from '../assets/Media/Business.png';
+import businessImg from '../assets/Media/Goals.svg';
+import businessImg2 from '../assets/Media/Confidence.svg';
+import businessImg3 from '../assets/Media/Stress.svg';
+import businessImg4 from '../assets/Media/peace.svg';
+import businessImg5 from '../assets/Media/Opportunity.svg'
 
 const productData = [
         {   
             id: 1,
-            name: "Insurance",
-            description: "Affordable coverage for a specific term, ideal for temporary needs.",
+            name: "Achievieing financial goals",
+            description: "Financial goals provide a clear roadmap for how to manage your money.Small wins build momentum, leading to bigger accomplishments like financial independence.",
             image: businessImg,
-            link : "/life-insurance"
         },
         {
             id: 2,
-            name: "Investments",
-            description: "Lifetime coverage with cash value accumulation.",
-            link : "/annuities"
+            name: "Gain financial confidence",
+            description: "Financial confidence reduces fear and emotional decision-making, empowering you to take calculated risks.",
+            image: businessImg2,
         },
         {
             id: 3,
-            name: "Retirement",
-            description: "help clients save for retirement by selling various tax-advantaged accounts and funds.",
+            name: "Reduce Stress Levels",
+            description: "A well-prepared financial plan can eliminate the worry of living paycheck to paycheck, offering a sense of security.",
+            image: businessImg3,
             link : "/investments"
+        },
+        {
+            id: 4,
+            name: "Financial Peace of Mind",
+            image: businessImg4,
+            description: "With financial peace of mind, you can focus on other aspects of life, without worrying about money."
+        },
+          {
+            id: 5,
+            name: "Ability to Seize Investment Opportunities",
+            description: "Financial planning allows you to allocate funds specifically for investments, ensuring you can take action when the right chance arises.",
+            image: businessImg5,
         }
 
     ];
